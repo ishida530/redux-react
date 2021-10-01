856ef40675f829beac493ef0c77c09a76526fc01
ishida530 <63319022+ishida530@users.noreply.github.com>
2021-10-01 15:58:10 +0200

    Add files via upload

--- src/components/ContentPage/Basket/Basket.js
+++ src/components/ContentPage/Basket/Basket.js
@@ -1,11 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import shortid from 'shortid';
+import { basketList } from '../../../redux/store'
+const Basket = ({ basketList }) => {
+    let checkerSameId;
+    const [filterBasket, setFilterBasket] = useState([])
+    console.log('basketList', basketList)
+    console.log('checkerSameId', checkerSameId)
 
-const Basket = () => {
+
+    // let oldKey = '';
+    // let iteration = -1;
+    // const arr = [];
+
+    // basketList.forEach(el=>{
+    //     if (el.key != oldKey) {
+    //         arr.push([el]);
+    //         iteration++;
+    //         oldKey = el.key;
+    //     } else {
+    //         arr[iteration] = [...arr[iteration],el];
+    //     }
+    // })
+
+
+    const li = (item) => (
+        <li key={shortid()}>
+            {item.title}<input type="number"  />
+        </li>
+    )
     return (
         <div>
-            basket
+            <ol>
+
+                {/* {
+                    arr.map((el,index)=>{
+                        let amount = 0;
+                        el.forEach(elInsideEl=>{
+                            amount++;
+                        })
+                        return (
+                            <li>{arr[index][0].title}-{amount}</li>
+                        )
+                    }
+                )
+
+                } */}
+                {basketList.map(item => li(item)
+
+                    )}
+
+                {/* {basketList.filter((thing, index) => {
+  const _thing = JSON.stringify(thing);
+  return index === basketList.findIndex(obj => {
+    return JSON.stringify(obj) === _thing;
+  })
+})} */}
+
+
+
+
+            </ol>
+
+
+
+
+
         </div>
     )
 }
-
-export default Basket
+const mapStateToProps = state => ({
+    basketList: basketList(state)
+})
+export default connect(mapStateToProps)(Basket)
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,20 +6,27 @@ import shortid from "shortid";
 
 //selectors
 
+export const countInBasket = (state) => state.basket.length;
+export const basketList = (state) => state.basket;
+
+
 
 const createActionName = name => `app/books/${name}`
 // action names
 const UPDATE_BOOKS = createActionName('UPDATE_BOOKS')
-const ADD_ID_FOR_BOOK = createActionName('ADD_ID_FOR_BOOK')
-
+const ADD_BOOK_TO_BASKET= createActionName('ADD_BOOK_TO_BASKET')
+const INCREMENT_COUNT_IN_BASKET=createActionName('INCREMENT_COUNT_IN_BASKET')
 // action creators
 export const updateBooks = payload => ({ type: UPDATE_BOOKS, payload })
-export const addIdSingleBook = payload => ({ type: ADD_ID_FOR_BOOK, payload })
+export const addItemToBasket = payload => ({ type: ADD_BOOK_TO_BASKET, payload })
+export const incrementItemBasket = payload => ({ type: INCREMENT_COUNT_IN_BASKET, payload })
+
 
 
 
 
 const initialState = {
+    basket:[]
 }
 
 export const fetchbooks = () => {
@@ -31,26 +38,43 @@ export const fetchbooks = () => {
                 { ...item, key: shortid().toString() })
             );
 
-            dispatch(updateBooks(newData))
+           dispatch(updateBooks(newData))
         } catch (err) {
             console.error(err)
         }
     }
 }
+export const checkBasket = (book,state) => {
+    console.log('state',state)
+    console.log('book',book)
+    return async (dispatch) => {
 
+
+        // const countItem = state.basket.filter(item => {
+        //     console.log('item.key',item.key)
+        //     console.log('book.key',book.key)
+        //     return item.key === book.key
+        // })
+
+    // console.log('countItem.length',countItem.length)
+    // state.basket.length > 1 ?  incrementItemBasket() : dispatch(addItemToBasket(book))
+    dispatch(addItemToBasket(book))
+
+}
+}
 
 const reducer = (state = initialState, { type, payload }) => {
     switch (type) {
-
         case UPDATE_BOOKS:
             return { ...state, books: [...payload] }
-        case ADD_ID_FOR_BOOK:
-            return {
-                ...state, books: state.books.forEach(e => {
-                    e.id = payload
+        case ADD_BOOK_TO_BASKET:
+        return  { ...state,basket: [...state.basket, payload] }
+        case INCREMENT_COUNT_IN_BASKET:
+            return  { ...state,basket: [...state.basket, payload.key++] }
+            // return  { ...state,basket: [...state.basket.filter(item=>item.key===payload.key)] }
+     
 
-                })
-            }
+
         default:
             return state
     }
@@ -64,5 +88,6 @@ const enhancer = composeEnhancers(
 );
 const store = createStore(reducer, initialState, enhancer)
 
-
 export default store
+
+
